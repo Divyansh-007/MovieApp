bd8b34f72b6615de5507341306fda95aec347daf
Divyansh-007 <61491200+Divyansh-007@users.noreply.github.com>
2021-05-10 11:53:33 +0530

    Create store , dispatch actions and subscribe to the store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers/index';
+
+const store = createStore(movies);
+// console.log('store',store);
+// console.log('Before State',store.getState());
+
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies: [{name: 'Iron Man'}]
+// });
+
+// console.log('After State',store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store = {store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
