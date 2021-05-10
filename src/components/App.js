a81ea7599d39576b1fd575a6cf438b535ed023f8
Divyansh-007 <61491200+Divyansh-007@users.noreply.github.com>
2021-05-10 12:14:47 +0530

    Diversify actions and improve code

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import data from '../data';
+import { addMovies } from '../actions'
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 
@@ -13,10 +14,7 @@ class App extends React.Component {
 
     // api call
     // dispatch action
-    this.props.store.dispatch({
-      type: 'ADD_MOVIES',
-      movies: data
-    });
+    this.props.store.dispatch(addMovies(data));
 
     console.log('STATE',this.props.store.getState());
   }
